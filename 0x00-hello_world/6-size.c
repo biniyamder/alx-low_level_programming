964904c5da7fa65b50ed4df1fab6f271054b6aa5
biniyamder <biniyamdereje1@gmail.com>
2022-03-10 15:36:37 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char charactertype;
 	int integertype;
@@ -12,11 +12,11 @@ int main (void)
 	long long int longlongintegertype;
 	float floattype;
 
-	printf("size of a char: %d byte(s)\n",sizeof(charactertype));
-	printf("size of an int: %d byte(s)\n",sizeof(integertype));
-	printf("size of a long int: %d byte(s)\n",sizeof(longintegertype));
-	printf("size of a long long int: %d byte(s)\n",sizeof(longlongintegertype));
-	printf("size of a float: %d byte(s)\n",sizeof(floattype));
+	printf("size of a char: %d byte(s)\n", sizeof(charactertype));
+	printf("size of an int: %d byte(s)\n", sizeof(integertype));
+	printf("size of a long int: %d byte(s)\n", sizeof(longintegertype));
+	printf("size of a long long int: %d byte(s)\n", sizeof(longlongintegertype));
+	printf("size of a float: %d byte(s)\n", sizeof(floattype));
 
 	return (0);
 }
