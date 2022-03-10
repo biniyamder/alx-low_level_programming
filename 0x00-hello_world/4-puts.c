707ac4e3d6bbdb1f3216ba3a3c04f40631b634cb
biniyamder <biniyamdereje1@gmail.com>
2022-03-10 14:40:10 -0800

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
-{ 
-	char str1[52];
+{char *str1[52];
 	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 	return (0);
