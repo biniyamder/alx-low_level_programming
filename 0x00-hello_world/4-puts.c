216880b5eacceb8c0e75816c71bf78ff262bc51a
biniyamder <biniyamdereje1@gmail.com>
2022-03-10 14:30:57 -0800

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 int main(void)
 { 
 	char str1[52];
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-	puts(str1);
 
+	strcpy(str1,"\"Programming is like building a multilingual puzzle");
+	puts(str1);
 	return (0);
 }
