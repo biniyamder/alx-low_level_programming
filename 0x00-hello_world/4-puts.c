b1b4b2f195c96ea60e9c4cc7c262a2e6346275ab
biniyamder <biniyamdereje1@gmail.com>
2022-03-10 14:51:12 -0800

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  * Return: Always 0 (success)
  */
 int main(void)
-
 {
 	char str1[52];
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	strcpy(str1 , "\"Programming is like building a multilingual puzzle");
 
 	puts(str1);
 	return (0);
