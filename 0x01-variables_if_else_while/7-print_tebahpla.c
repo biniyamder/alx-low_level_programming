ca61d8371d728504a9369ebb2688bedc77d06512
biniyamder <biniyamdereje1@gmail.com>
2022-03-11 05:25:55 -0800

    print alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
