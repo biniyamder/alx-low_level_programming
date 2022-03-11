44a776cd4d5345a94fff3ce83503a8ebc3befb59
biniyamder <biniyamdereje1@gmail.com>
2022-03-11 04:29:57 -0800

    exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'|| (ch != 'q' && ch != 'e'); ch++)
+	for (ch = 'a'; ch <= 'z' || (ch != 'q' && ch != 'e'); ch++)
 	{
 		putchar(ch);
 	}
