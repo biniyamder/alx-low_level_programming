dd06e7051817907a5d1dd06ba0e3297746434a09
biniyamder <biniyamdereje1@gmail.com>
2022-03-11 05:45:51 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	int n = 0X0;
-	char ch = 0Xa;
+	unsigned char n = 0X0;
+	unsigned char ch = 0Xa;
 	
 	for (n = '0'; n <= 10; n++)
 	{
-		putchar(n);
+		putchar(%x);
 	}
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+		putchar(%x);
 	}
 	putchar('\n');
 
