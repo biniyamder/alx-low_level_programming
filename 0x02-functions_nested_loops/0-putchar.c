38110dd793522ea6838cf3a13c76442f3df6e169
biniyamder <biniyamdereje1@gmail.com>
2022-03-16 11:54:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
 int main (void)
 {
 	putchar(95);
-    	putchar(112);
-    	putchar(117);
-    	putchar(116);
-    	putchar(99);
-    	putchar(104);
-    	putchar(97);
-    	putchar(114);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar(32);
 
     return (0);
 }
