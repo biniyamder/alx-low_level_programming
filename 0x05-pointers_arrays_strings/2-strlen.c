1d13585e903c3720f99e27770645163b3e90b1ef
biniyamder <biniyamdereje1@gmail.com>
2022-03-21 12:53:37 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - swaps values of two integers
+ *
+ * @s: integer
+ * Return; Always 0
+ */
+int _strlen(char *s)
+{
+	putchar(strlen(s));
+}
