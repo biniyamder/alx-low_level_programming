2ad3babe0fb59e52eb7e69e3c4fe0c1f5b3e59bb
biniyamder <biniyamdereje1@gmail.com>
2022-03-21 13:22:53 -0700

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: input string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	char str[];
+
+	*str = str;
+}
