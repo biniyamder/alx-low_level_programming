000c3e714d7a21c3172664751db37716f2c4fd87
biniyamder <biniyamdereje1@gmail.com>
2022-03-21 12:12:28 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#inlcude "main.h"
+/**
+ * reset_to_98 - a pointer to an int a parameter
+ *
+ * *n: integer input
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *n;
+
+	in = &n;
+
+	putchar(*in);
+
+	return (0);
+}
