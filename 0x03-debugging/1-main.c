39721c5a4f0608c964e6f98161126177beba627b
biniyamder <biniyamdereje1@gmail.com>
2022-03-24 12:47:12 -0700

    infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - causing the output to go into an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
