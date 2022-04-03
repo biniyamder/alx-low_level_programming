3bbcad1b3bd3a68cd719c74916ca6bcc4fafc399
biniyamder <biniyamdereje1@gmail.com>
2022-04-03 00:29:03 -0700

    strncpY

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strncpy - copies a string
+ *@dest: first string
+ *@src: second string
+ *@n: the number of bytes to use
+ *Return: string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, k = 0;
+
+	while (n > k)
+	{
+		if (src[k] == '\0')
+		{
+			for (; k < n; k++)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+		else
+		{
+			dest[i] = src[k];
+			k++;
+			i++;
+		}
+	}
+
+	return (dest);
+}
