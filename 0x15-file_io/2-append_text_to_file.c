4e5a2b717ccb8c58e9be358d7043d6631b1268c7
biniyamder <biniyamdereje1@gmail.com>
2022-05-31 01:25:56 -0700

    function that appends text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,30 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-  * append_text_to_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * append_text_to_file - Append text to the end of a file
+ * @filename: name of file
+ * @text_content: string to add to end of file
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, status, i;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
-	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	status = write(fd, text_content, i);
+	if (status == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
